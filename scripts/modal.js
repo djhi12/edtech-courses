9aa8529efbf7f6113b55a14fcb6f390b5adda94c
djhi12 <ido20002@byui.edu>
2023-08-31 14:32:45 +0800

    Initial commit

--- scripts/modal.js
+++ scripts/modal.js
@@ -8,37 +8,58 @@ images.forEach((image, index) => {
     });
 });
 
-closeBtns.forEach((closeBtn) => {
+modals.forEach((modal, index) => {
+    const closeBtn = modal.querySelector('.close'); // Find the close button within this modal
     closeBtn.addEventListener('click', () => {
-        closeModal();
+        closeModal(index); // Pass the index of the modal to be closed
     });
-});
 
-modals.forEach((modal) => {
     modal.addEventListener('click', (event) => {
         if (event.target === modal) {
-            closeModal();
+            closeModal(index); // Pass the index of the modal to be closed
         }
     });
 });
 
+function closeModal(index) {
+    modals[index].style.display = 'none';
+}
+
 function showModal(index) {
     const modalContent = [
-        "Content for image 1.",
-        "Content for image 2.",
-        "Content for image 3.",
+        // html
+        `
+        <p><strong>HTML</strong> stands for "Hypertext Markup Language." It is the standard markup language used to create and structure content on the World Wide Web (the internet). HTML is the foundation of web pages and is used to define the structure and layout of a web document, such as text, images, links, forms, and other multimedia elements.</p>
+        
+        <p>By the end of this course, students will be able to:</p>
+        
+        <ul>
+        <li>Understand HTML Basics</li>
+        <li>Create Structured Content</li>
+        <li>Apply Hyperlinks</li>
+        <li>Embed Multimedia</li>
+        <li>Construct Forms</li>
+        <li>Implement Semantic Markup</li>
+        <li>Integrate CSS Styles</li>
+        <li>Adhere to Best Practices</li>
+        <li>Debug and Validate</li>
+        <li>Collaborate and Share</li>
+        </ul>
+        `,
+
+        // CSS
+        `<p>Content for image 2.</p>`,
+
+        `<p>Content for image 3.</p>`,
         // Add more content entries for each image
+
+
     ];
 
     const content = modalContent[index];
 
-    const modalContentElement = modals[index].querySelector('.modal-content p');
-    modalContentElement.textContent = content;
+    const modalContentElement = modals[index].querySelector('.modal-content');
+    modalContentElement.innerHTML = content;
     modals[index].style.display = 'block';
 }
 
-function closeModal() {
-    modals.forEach((modal) => {
-        modal.style.display = 'none';
-    });
-}
